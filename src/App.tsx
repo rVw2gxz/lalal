069bffb96a221245973fdcb1e2a4fdb4db223ee8
Eliseenko Alex <scanel2004@gmail.com>
2024-07-02 10:13:48 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className='wrap'>
         <div className='box flex1'>
-          
+            s
         </div>
        
     </div>
