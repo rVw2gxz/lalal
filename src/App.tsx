fc8b562cef4f253188328e66844b647efe989d2c
Eliseenko Alex <scanel2004@gmail.com>
2024-07-02 09:52:13 +0700

    delet app

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,7 @@ function App() {
   return (
     <div className='wrap'>
         <div className='box flex1'>
-          <ul>
-            <li>
-              <a href="">1</a>
-              <span>Привет мир</span>
-            </li>
-            <li>
-              <a href="">2 </a>
-              <span>Привет мир</span>
-            </li>
-            <li>
-              <a href="">3</a>
-              <span>Привет мир</span>
-            </li>
-            <li>
-              <a href="">4</a>
-              <span>Привет мир</span>
-            </li>
-            <li>
-              <a href="">5</a>
-              <span>Привет мир</span>
-            </li>
-          </ul>
+          
         </div>
        
     </div>
